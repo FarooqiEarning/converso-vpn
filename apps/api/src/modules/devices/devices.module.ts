f82acb956e6cb2e70fc20cd0734a479831518449
FarooqiEarning <farooqi.earning.official@gmail.com>
2026-05-16 14:56:38 +0500

    fix: resolve DevicesService dependency injection error by importing WireGuardModule

--- apps/api/src/modules/devices/devices.module.ts
+++ apps/api/src/modules/devices/devices.module.ts
@@ -8,9 +8,13 @@ import { DevicesController } from './devices.controller';
 import { DevicesService } from './devices.service';
 import { WireGuardPeer } from '../wireguard/entities/wireguard-peer.entity';
 import { VpnNode } from '../vpn-nodes/entities/vpn-node.entity';
+import { WireGuardModule } from '../wireguard/wireguard.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([WireGuardPeer, VpnNode])],
+  imports: [
+    TypeOrmModule.forFeature([WireGuardPeer, VpnNode]),
+    WireGuardModule,
+  ],
   controllers: [DevicesController],
   providers: [DevicesService],
   exports: [DevicesService],
